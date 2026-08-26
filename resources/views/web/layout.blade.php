3913c12a2100d17640206c91c17bf2a06caee5da
Fuad Hasanli <fhesenli92@gmail.com>
2026-08-26 23:40:42 +0400

    cc

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -8,7 +8,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;1,9..40,400&family=Fraunces:opsz,wght@9..144,600;9..144,700&display=swap" rel="stylesheet">
-    @if (file_exists(public_path('hot')) || file_exists(public_path('build/manifest.json')))
+    {{-- Production: static CSS (serverdə Node/Vite lazım deyil). Local `npm run dev` → public/hot. --}}
+    @if (file_exists(public_path('hot')))
         @vite(['resources/css/web.css'])
     @else
         <link rel="stylesheet" href="{{ asset('css/web.css') }}?v={{ @filemtime(public_path('css/web.css')) }}">
