2eb7f7cd36b3363c98805356d2e05fcf0b5873c6
Fuad Hasanli <fhesenli92@gmail.com>
2026-09-05 00:12:17 +0400

    cc

--- config/app_locales.php
+++ config/app_locales.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => 36,
+    'version' => 37,
     'default' => env('APP_DEFAULT_LOCALE', 'az'),
     'supported' => array_filter(explode(',', env('APP_SUPPORTED_LOCALES', 'az,en,ru'))),
     'labels' => [
